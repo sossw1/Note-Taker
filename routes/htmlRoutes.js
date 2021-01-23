a520631276eddddc63ac18275b14f542bfdbfdf6
sossw1 <sossw1@gmail.com>
2021-01-23 15:54:20 -0500

    GET route '/'

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,4 +5,8 @@ module.exports = function(app) {
     app.get('/notes', function(req,res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 }
