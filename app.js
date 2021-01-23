359359959367332867845ffbce516b02fadd41fa
sossw1 <sossw1@gmail.com>
2021-01-22 20:57:48 -0500

    Add routes files

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Paths
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
 // Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
---
+++ routes/apiRoutes.js
@@ -0,0 +1,10 @@
+module.exports = function(app) {
+    // API GET Requests
+    
+
+    // API POST Requests
+
+
+    // API DELETE Requests
+
+}
