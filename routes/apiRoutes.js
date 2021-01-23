abb9c695a94aa6f059e0d0f5a79056d983fcd456
sossw1 <sossw1@gmail.com>
2021-01-23 15:13:59 -0500

    GET path /api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
+let noteData = require('../db/db.json');
+
 module.exports = function(app) {
     // API GET Requests
-    
+        //GET "/api/notes" gets all notes from db
+        app.get('/api/notes', function(req,res) {
+            res.json(noteData);
+        });
 
     // API POST Requests
-
+        //POST "/api/notes" saves a note to db given data (note)
 
     // API DELETE Requests
+        //DELETE "api/notes" deletes a note from the db given id (url encoded)
 
 }
