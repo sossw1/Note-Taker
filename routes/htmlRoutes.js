e96f7111ea492c8a8a7c1efdf73d1acd156c8343
sossw1 <sossw1@gmail.com>
2021-01-23 15:36:01 -0500

    GET path for /notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,8 @@
+const path = require('path');
+
 module.exports = function(app) {
     // GET Requests
-
+    app.get('/notes', function(req,res) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
 }
