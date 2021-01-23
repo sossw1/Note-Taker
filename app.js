3a2e38c386da66e8a4d7bd5623da77f0c496fcb1
sossw1 <sossw1@gmail.com>
2021-01-22 20:38:24 -0500

    Middleware

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
